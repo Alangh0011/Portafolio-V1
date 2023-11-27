cf47baef5c15a8a0fffb2ecf09a066708f0a44af
agomezh1901@alumno.ipn.mx <agomezh1901@alumno.ipn.mx>
2023-11-26 19:09:38 -0600

    Actualiza configuración de despliegue para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import ViteReact from '@vitejs/plugin-react';
 
 export default defineConfig({
+  build: {
+    outDir: 'docs', // Carpeta de salida para producción
+  },
   plugins: [ViteReact()],
   server: {
     fs: {
